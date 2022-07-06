930ee0a725e06af3dafd9db1aa2e1e5d444182b0
lazarolopez <lazaro.e.lopez@gmail.com>
2022-07-06 16:00:32 -0500

    ordering ex

---
+++ order_by_exercises.sql
@@ -0,0 +1,42 @@
+USE employees;
+DESCRIBE employees;
+-- Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by first name. In your comments, answer: What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
+SELECT *
+	FROM employees
+    WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+    ORDER BY first_name;
+-- irena reutenauer 
+-- vidya awdeh
+    
+-- Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by first name and then last name. In your comments, answer: What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
+SELECT *
+	FROM employees
+    WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+    ORDER BY first_name, last_name;
+-- irena acton, vidya zwiezig
+
+-- Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by last name and then first name. In your comments, answer: What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
+SELECT *
+	FROM employees
+    WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+    ORDER BY last_name, first_name;
+-- irena acton, mya zyda
+-- Write a query to to find all employees whose last name starts and ends with 'E'. Sort the results by their employee number. Enter a comment with the number of employees returned, the first employee number and their first and last name, and the last employee number with their first and last name.
+SELECT *
+	FROM employees
+    WHERE last_name like'E%' 
+    ORDER BY emp_no;
+-- Ramzi Erde, Dharmaraja ertl
+-- Write a query to to find all employees whose last name starts and ends with 'E'. Sort the results by their hire date, so that the newest employees are listed first. Enter a comment with the number of employees returned, the name of the newest employee, and the name of the oldest employee.
+SELECT *
+	FROM employees
+    WHERE last_name like'E%' 
+    ORDER BY hire_date ASC;
+-- (oldest)Cristinel Erdi,(Newest)PhillipEppinger
+-- Find all employees hired in the 90s and born on Christmas. Sort the results so that the oldest employee who was hired last is the first result. Enter a comment with the number of employees returned, the name of the oldest employee who was hired last, and the name of the youngest employee who was hired first.
+SELECT *
+	FROM employees
+    WHERE birth_date like '%-12-25'
+    and hire_date BETWEEN '1989-12-31' and '2000-01-01'
+    ORDER BY birth_date ASC, hire_date DESC;
+-- KhunBernini, DouadiPettis
