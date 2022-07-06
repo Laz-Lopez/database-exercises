0a37bbf02ffb759e78c8a050369d4f7432cb7a23
lazarolopez <lazaro.e.lopez@gmail.com>
2022-07-06 09:58:18 -0500

    tables ex, tables are hard missing final solution

---
+++ tables_exercises.sql
@@ -0,0 +1,18 @@
+USE employees;
+SHOW TABLES;
+DESCRIBE employees;
+DESCRIBE departments;
+DESCRIBE salaries;
+DESCRIBE dept_emp;
+DESCRIBE dept_manager;
+
+-- Which table(s) do you think contain a numeric type column? 
+-- Salaries,employees
+-- Which table(s) do you think contain a string type column? 
+-- employees, departments
+-- Which table(s) do you think contain a date type column? 
+-- Salaries, Dept. Manager, employees.
+-- What is the relationship between the employees and the departments tables? 
+-- Dept_emp they boths show employee numbers and starts dates
+-- What is the relationship between the employees and the departments tables?
+-- varchar?
